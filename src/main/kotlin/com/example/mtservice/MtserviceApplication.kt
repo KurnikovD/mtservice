8bd06b098a63ba0757748e4abac9f8fba0bd339d
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2022-12-22 16:15:11 +0300

    init project

---
+++ src/main/kotlin/com/example/mtservice/MtserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mtservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MtserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MtserviceApplication>(*args)
+}
