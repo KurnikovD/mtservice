934e57479b329a28774ca46ad2e3cd1cd26a030e
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2022-12-22 19:13:33 +0300

    fix security configuration

--- src/main/kotlin/com/example/mtservice/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/com/example/mtservice/service/impl/UserServiceImpl.kt
@@ -4,16 +4,19 @@ import com.example.mtservice.dto.UserDto
 import com.example.mtservice.entity.UserEntity
 import com.example.mtservice.repository.UserRepository
 import com.example.mtservice.service.UserService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
 ): UserService {
     override fun signup(userDto: UserDto) {
         val user = UserEntity(
             userDto.username,
-            userDto.password
+            passwordEncoder.encode(userDto.password)
+
         )
         userRepository.save(user)
     }
